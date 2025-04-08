b7d94dc62677f8535a786553fe6a54b01aabf1d6
ManuelROsvaldo <manuel23osvaldo@gmail.com>
2025-04-08 16:12:11 -0400

    Diseño-Completado

---
+++ Proyecto__Final/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto__Final
+{
+    static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+
+                FormLogin loginForm = new FormLogin();
+                if (loginForm.ShowDialog() == DialogResult.OK)
+                {
+                    Application.Run(new FormClientes()); // este es el formulario principal
+                }
+        }
+    }
+
+}
